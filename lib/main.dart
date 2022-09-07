51b463272e9a23925fe916b10f01b58ec3714b9a
amirr-dotcom <71167542+amirr-dotcom@users.noreply.github.com>
2022-09-07 18:04:45 +0530

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,28 @@
 
 import 'package:flutter/material.dart';
+import 'package:in_app_update/in_app_update.dart';
 import 'package:jaferya_squard/splash_screen_view.dart';
+
+
+
+Future<void> checkForUpdate() async {
+  InAppUpdate.checkForUpdate().then((info) {
+    if(info?.updateAvailability ==
+        UpdateAvailability.updateAvailable){
+      InAppUpdate.performImmediateUpdate()
+          .catchError((e) => print("fff"));
+    }
+
+  }).catchError((e) {
+
+  });
+}
 
 
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
-
+  await checkForUpdate();
   runApp(
       const MyApp()
   );
